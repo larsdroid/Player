38f374fb4f37fddf6c28929b95e0801c5129d3d1
Lars Willemsens <lars@willemsens.org>
2018-05-10 20:20:52 +0200

    Album fragment: cover art updated after it is fetched from the web API.

--- app/src/main/java/org/willemsens/player/util/StringFormat.java
+++ app/src/main/java/org/willemsens/player/util/StringFormat.java
@@ -1,7 +1,11 @@
 package org.willemsens.player.util;
 
 public class StringFormat {
-    public static String formatToSongLength(int songLengthSeconds) {
-        return String.format("%d:%02d", songLengthSeconds / 60, songLengthSeconds % 60);
+    public static String formatToSongLength(Integer songLengthSeconds) {
+        if (songLengthSeconds != null) {
+            return String.format("%d:%02d", songLengthSeconds / 60, songLengthSeconds % 60);
+        } else {
+            return "--:--";
+        }
     }
 }
--- app/src/main/java/org/willemsens/player/view/main/album/AlbumFragment.java
+++ app/src/main/java/org/willemsens/player/view/main/album/AlbumFragment.java
@@ -1,13 +1,18 @@
 package org.willemsens.player.view.main.album;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -21,16 +26,22 @@ import org.willemsens.player.model.Image;
 import org.willemsens.player.persistence.AppDatabase;
 import org.willemsens.player.persistence.MusicDao;
 import org.willemsens.player.view.customviews.HeightCalculatedImageView;
+import org.willemsens.player.view.customviews.HeightCalculatedProgressBar;
 
 import static org.willemsens.player.musiclibrary.MusicLibraryBroadcastPayloadType.MLBPT_ALBUM_ID;
+import static org.willemsens.player.musiclibrary.MusicLibraryBroadcastType.MLBT_ALBUM_UPDATED;
 
 public class AlbumFragment extends Fragment {
     private Album album;
     private AlbumSongAdapter adapter;
     private MusicDao musicDao;
+    private AlbumUpdateReceiver updateReceiver;
 
     @BindView(R.id.album_image)
     HeightCalculatedImageView albumImage;
+
+    @BindView(R.id.album_image_progress_bar)
+    HeightCalculatedProgressBar progressBar;
 
     @BindView(R.id.song_list)
     RecyclerView songList;
@@ -39,6 +50,7 @@ public class AlbumFragment extends Fragment {
         final AlbumFragment theInstance = new AlbumFragment();
 
         theInstance.musicDao = AppDatabase.getAppDatabase(context).musicDao();
+        theInstance.updateReceiver = theInstance.new AlbumUpdateReceiver();
 
         Bundle args = new Bundle();
         args.putLong(MLBPT_ALBUM_ID.name(), albumId);
@@ -59,10 +71,8 @@ public class AlbumFragment extends Fragment {
             final long albumId = arguments.getLong(MLBPT_ALBUM_ID.name());
             this.album = musicDao.findAlbum(albumId);
 
-            final Image albumCover = musicDao.findImage(album.imageId);
-            final Bitmap bitmap = BitmapFactory.decodeByteArray(
-                    albumCover.imageData, 0, albumCover.imageData.length);
-            this.albumImage.setImageBitmap(bitmap);
+            fetchAndShowAlbumArt();
+
 
             Context context = view.getContext();
             this.songList.setLayoutManager(new LinearLayoutManager(context));
@@ -72,6 +82,21 @@ public class AlbumFragment extends Fragment {
             this.songList.setAdapter(this.adapter);
         }
         return view;
+    }
+
+    private void fetchAndShowAlbumArt() {
+        if (album.imageId != null) {
+            final Image albumCover = musicDao.findImage(album.imageId);
+            final Bitmap bitmap = BitmapFactory.decodeByteArray(
+                    albumCover.imageData, 0, albumCover.imageData.length);
+            this.albumImage.setImageBitmap(bitmap);
+
+            this.albumImage.setVisibility(View.VISIBLE);
+            this.progressBar.setVisibility(View.GONE);
+        } else {
+            this.albumImage.setVisibility(View.GONE);
+            this.progressBar.setVisibility(View.VISIBLE);
+        }
     }
 
     @Override
@@ -85,11 +110,41 @@ public class AlbumFragment extends Fragment {
     public void onResume() {
         super.onResume();
         adapter.registerPlayBackUpdateReceiver();
+        registerAlbumUpdateReceiver();
     }
 
     @Override
     public void onPause() {
+        unregisterAlbumUpdateReceiver();
         adapter.unregisterPlayBackUpdateReceiver();
         super.onPause();
+    }
+
+    private void registerAlbumUpdateReceiver() {
+        LocalBroadcastManager lbm = LocalBroadcastManager.getInstance(getContext());
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(MLBT_ALBUM_UPDATED.name());
+        lbm.registerReceiver(this.updateReceiver, filter);
+    }
+
+    private void unregisterAlbumUpdateReceiver() {
+        LocalBroadcastManager lbm = LocalBroadcastManager.getInstance(getContext());
+        lbm.unregisterReceiver(this.updateReceiver);
+    }
+
+    private class AlbumUpdateReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            final String intentAction = intent.getAction();
+            if (intentAction != null && intentAction.equals(MLBT_ALBUM_UPDATED.name())) {
+                long albumId = intent.getLongExtra(MLBPT_ALBUM_ID.name(), -1);
+                if (albumId == album.id && progressBar.getVisibility() == View.VISIBLE) {
+                    album = musicDao.findAlbum(album.id);
+                    fetchAndShowAlbumArt();
+                }
+            } else {
+                Log.e(getClass().getName(), "Invalid Intent received. Action: '" + intentAction + "'");
+            }
+        }
     }
 }
