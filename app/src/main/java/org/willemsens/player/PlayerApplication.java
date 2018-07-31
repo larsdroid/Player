a41b1f0ca9b933909f535ecbcfb4ee0d0ceb6ea7
Lars Willemsens <lars@willemsens.org>
2018-07-31 12:24:06 +0200

    Linked app with fabric.

--- app/src/main/java/org/willemsens/player/PlayerApplication.java
+++ app/src/main/java/org/willemsens/player/PlayerApplication.java
@@ -2,11 +2,16 @@ package org.willemsens.player;
 
 import android.app.Application;
 import android.os.StrictMode;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 public class PlayerApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        if (BuildConfig.USE_CRASHLYTICS) {
+            Fabric.with(this, new Crashlytics());
+        }
         StrictMode.enableDefaults();
     }
 }
