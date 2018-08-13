0797f4718ffd2be3f27e6109a2879976259237a1
Lars Willemsens <lars@willemsens.org>
2018-08-13 10:05:36 +0200

    (Thread)safer way to create singleton.

--- app/src/main/java/org/willemsens/player/persistence/AppDatabase.java
+++ app/src/main/java/org/willemsens/player/persistence/AppDatabase.java
@@ -30,12 +30,16 @@ public abstract class AppDatabase extends RoomDatabase {
 
     public static AppDatabase getAppDatabase(Context context) {
         if (INSTANCE == null) {
-            INSTANCE =
-                    Room.databaseBuilder(context.getApplicationContext(), AppDatabase.class, "app-database")
-                            // allow queries on the main thread.
-                            // TODO: Don't do this on a real app! See PersistenceBasicSample for an example.
-                            .allowMainThreadQueries()
-                            .build();
+            synchronized (AppDatabase.class) {
+                if (INSTANCE == null) {
+                    INSTANCE =
+                            Room.databaseBuilder(context.getApplicationContext(), AppDatabase.class, "app-database")
+                                    // allow queries on the main thread.
+                                    // TODO: Don't do this on a real app! See PersistenceBasicSample for an example.
+                                    .allowMainThreadQueries()
+                                    .build();
+                }
+            }
         }
         return INSTANCE;
     }
